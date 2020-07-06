2db9fb3f0253abab263b6d09da6e0735bd378eda
Rodolfo Martins Teobaldo <rodolfo.teobaldo@softplan.com.br>
2020-07-06 10:34:56 -0300

    Alteração model User

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,13 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 import { Type } from 'recife';
 
 @Type()
+@Entity('user')
 class UserModel {
+  @PrimaryGeneratedColumn()
   id?: number;
 
+  @Column()
   name?: string;
 
+  @Column()
   email?: string;
 
+  @Column()
   username?: string;
 }
 
